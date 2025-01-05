94a79045ed42d429db69ca2669d2b3f373a5f4aa
EPA <elio.palomba@namirial.com>
2025-01-06 00:49:41 +0100

    Add JSON support and error handling to Rocket application

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rust-rocket-backend
+
+To develop this app, run the following commands:
+
+```sh
+sudo snap install rustup
+rustup default stable
+cargo watch -x run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,67 @@
-#[macro_use] extern crate rocket;
+extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+
+use rocket::{catch, catchers, get, build, routes};
+use rocket::tokio::time::{sleep, Duration};
+use rocket::serde::json::{json, Value};
+
+use std::path::PathBuf;
+use std::collections::HashMap;
+
+#[catch(default)]
+fn default_catcher() -> Value {
+    json!({"error": "An error occurred"})
 }
 
-use rocket::tokio::time::{sleep, Duration};
+#[catch(404)]
+fn notfound_catcher() -> Value {
+    json!({"error": "Not found"})
+}
+
+#[catch(401)]
+fn unauthorized_catcher() -> Value {
+    json!({"error": "Unauthorized"})
+}
 
 #[get("/delay/<seconds>")]
-async fn delay(seconds: u64) -> String {
-    sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds", seconds)
+async fn delay(seconds: u64) -> Value {
+    // Return error if seconds > 5
+    if seconds < 5 {
+        sleep(Duration::from_secs(seconds)).await;
+        json!({"message": format!("Delay completed, waited for {} seconds", seconds)})
+    }
+    else {
+        json!({"error": "Delay time is too long", "details": "Seconds must be less than 5"})
+    }
+    
 }
 
+#[get("/display-info/<path..>?<query..>")]
+fn display_info(path: PathBuf, query: HashMap<String, String>) -> Value {
+    json!({"request_path": path, "query_params": query})
+}
+
+#[get("/")]
+fn index() -> Value {
+    json!({ 
+        "routes": [
+            "/",
+            "/delay/<seconds>",
+            "/display-info/<path..>?<query..>"
+        ],
+        "catchers": [
+            "404 - Not found",
+            "401 - Unauthorized",
+            "default - An error occurred"
+        ]
+    })
+}
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build()
-        .mount("/", routes![index, delay])
+    let _rocket = build()
+        .register("/", catchers![default_catcher, notfound_catcher, unauthorized_catcher])
+        .mount("/", routes![index, delay, display_info])
         .launch()
         .await?;
 
